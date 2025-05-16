6c505b3bc0dd860bf6625d563e0274062362b7d4
DaCodeChick <peepvtuber@gmail.com>
2025-05-15 19:55:54 -0700

    add CPtrList template class

--- common/CPtrList.h
+++ common/CPtrList.h
@@ -23,3 +23,48 @@ protected:
 	void Preallocate(uint inSize, uint inCount);
 	void Preallocate(uint inSize);
 };
+
+template <class T> class CPtrList : public CVoidPtrList
+{
+public:
+	void AddBackItem(T *inPtr)
+	{
+		CVoidPtrList::AddBackItem(inPtr);
+	}
+	void AddFrontItem(T *inPtr)
+	{
+		CVoidPtrList::AddFrontItem(inPtr);
+	}
+	T *AddItem(T *inPtr, uint inSize)
+	{
+		return (T *)CVoidPtrList::AddItem(inPtr, inSize);
+	}
+	T *AddItem(T *inPtr)
+	{
+		return (T *)CVoidPtrList::AddItem(inPtr);
+	}
+	T *InsertItem(T *inOffset, T *inPtr, uint inSize)
+	{
+		return (T *)CVoidPtrList::InsertItem(inOffset, inPtr, inSize);
+	}
+	T *InsertItem(T *inPtr, uint inIndex, byte inFillValue)
+	{
+		return (T *)CVoidPtrList::InsertItem(inPtr, inIndex, inFillValue);
+	}
+	bool IsInList(const T *inPtr) const
+	{
+		return CVoidPtrList::IsInList(inPtr);
+	}
+	T *PopFront()
+	{
+		return (T *)CVoidPtrList::PopFront();
+	}
+	T *RemoveItem(T *inPtr, uint inSize)
+	{
+		return (T *)CVoidPtrList::RemoveItem(inPtr, inSize);
+	}
+	bool RemoveItem(T *inPtr)
+	{
+		return CVoidPtrList::RemoveItem(inPtr);
+	}
+};
