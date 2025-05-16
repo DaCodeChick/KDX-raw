8565ea92d20c5f449ae078a7bd302ebf9bd03115
DaCodeChick <peepvtuber@gmail.com>
2025-05-16 13:38:30 -0700

    ConfigureSocket

--- common/URegularTransport.cpp
+++ common/URegularTransport.cpp
@@ -12,7 +12,7 @@ struct SRegularTransport
 {
 	SRegularTransport *next;
 	uint tag;
-	SOCKET sock;
+	TSocket sock;
 	HWND hwnd;
 	int id;
 	TRHandlerProc msgProc;
@@ -30,4 +30,25 @@ struct SRegularTransport
 	bool isTemporary;
 };
 
+// KDXClient.exe: 00500200
 static bool gTRInitialized = false;
+
+// KDXClient.exe: 0046f6a0
+void __cdecl URegularTransport::ConfigureSocket(TSocket inSocket)
+{
+	char local_c[4];
+
+	local_c[0] = '\0';
+	local_c[1] = '\0';
+	local_c[2] = '\x02';
+	local_c[3] = '\0';
+	setsockopt(inSocket, SOL_SOCKET, SO_SNDBUF, local_c, 4);
+	setsockopt(inSocket, SOL_SOCKET, SO_RCVBUF, local_c, 4);
+	local_c[0] = '\x01';
+	local_c[1] = '\0';
+	local_c[2] = '\0';
+	local_c[3] = '\0';
+	setsockopt(inSocket, IPPROTO_TCP, SO_DEBUG, local_c, 4);
+	setsockopt(inSocket, SOL_SOCKET, SO_KEEPALIVE, local_c, 4);
+	return;
+}
--- common/URegularTransport.h
+++ common/URegularTransport.h
@@ -2,9 +2,17 @@
 
 #include "typedefs.h"
 
+#ifdef _WIN32
+typedef SOCKET TSocket;
+#else
+typedef int TSocket;
+#endif // _WIN32
+
 struct SRegularTransport;
 typedef void (*TRHandlerProc)(ushort inListenPort, SRegularTransport *inTpt, int inType);
 
 class EXPORT URegularTransport
 {
+public:
+	static void ConfigureSocket(TSocket inSocket);
 };
